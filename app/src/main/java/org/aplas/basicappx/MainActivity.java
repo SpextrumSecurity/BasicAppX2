9e98def72d301231dcf57b7321c18d813c87af19
SpextrumSecurity <farisikhlasulhaq@yahoo.co.id>
2021-09-10 15:27:46 +0700

    Define fields and methods in Activity

--- app/src/main/java/org/aplas/basicappx/MainActivity.java
+++ app/src/main/java/org/aplas/basicappx/MainActivity.java
@@ -1,8 +1,15 @@
 package org.aplas.basicappx;
 
 import androidx.appcompat.app.AppCompatActivity;
+import java.text.DecimalFormat;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    private Distance dist = new Distance();
+    private Weight weight = new Weight();
+    private Temperature temp = new Temperature();
+    private Button convertBtn;
+    private EditText inputTxt;
+    private EditText outputTxt;
+    private Spinner unitOri;
+    private Spinner unitConv;
+    private RadioGroup unitType;
+    private CheckBox roundBox;
+    private CheckBox formBox;
+    private ImageView imgView;
+
+    protected double convertUnit(String type,String oriUnit,String convUnit,double value){
+        if(type.equals("Temperature")){
+            return temp.convert(oriUnit,convUnit,value);
+        }else if(type.equals("Distance")){
+            return dist.convert(oriUnit,convUnit,value);
+        }else{
+            return weight.convert(oriUnit,convUnit,value);
+        }
+    }
+    protected String strResult (double val, boolean rounded){
+        if (rounded) {
+            DecimalFormat f = new DecimalFormat("#.##");
+            return f.format(val);
+        } else {
+            DecimalFormat f = new DecimalFormat("#.#####");
+            return f.format(val);
+        }
     }
 }
